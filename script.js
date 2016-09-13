a2c0c03c0e938083762a49cac202701614c65d7a
Chaz-Mykl <chaz.mykl@gmail.com>
2016-09-12 23:47:00 -0600

    Phase one tasks complete. Working on phase two - disabling submit button.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// When a mouse click occurs with the submit button, the "function (e)..." stops the 'submit' button from trying to 'submit a form'
+$('.new-item-form--submit').on('click', function (e){
+  e.preventDefault();
 
+  // Turns the input field in a variable, and gives the value of the field
+  var item = $('.new-item-form--name').val();
+
+  // Adds the created items --> ${item} - to the section 'item-list', newest input on top
+  $('.item-list').prepend(`<article class="items">${item}</article>`);
+
+
+// After an item is submitted, this clears the input
+  $('.new-item-form--name').val("");
+})
+
+// Disables submit button if there's no text in the input
